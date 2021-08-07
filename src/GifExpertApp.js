cf3dfeb4bed0ce1b03aca1da7374f7728914a6d6
cgcarlosg <cgcarlosg@hotmail.com>
2021-08-06 19:41:46 -0500

    first 2 components

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,24 +1,28 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import {AddCategory} from './components/AddCategory';
+import GifGrid from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories]= useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+    const [categories, setCategories]= useState(['One Punch']);
 
-    const handleAdd = () => {
-        setCategories(cats => [...cats, 'Hunter']);
-    }
+    //const handleAdd = () => {
+    //    setCategories(cats => [...cats, 'Hunter']);
+    //}
     return (
     <>
         <h2>GifExpertApp</h2>
+        <AddCategory setCategories = {setCategories}/>
         <hr/>
 
-        <button onClick={handleAdd}>Agregar</button>
 
         <ol>
         {
-          categories.map((category) =>{
-              return <li key={category}>{category}</li>
-          })
+          categories.map((category) =>
+              <GifGrid 
+              key = {category}
+              category={category}/>
+          )
         }
         </ol>
         
---
+++ src/components/GifGrid.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react'
+
+export const GifGrid = ({category}) => {
+
+   
+
+    useEffect(() => {
+        getGifs();
+    }, [])
+
+    const getGifs = async () => {
+
+        const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=zQrHcrBtgmuJwF6U00i5PbVddUQvyaRS'
+        const resp = await fetch(url);
+        const {data} = await resp.json();
+
+        const gifs = data.map(img => {
+
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images?.downsized_medium.url
+            }
+        })
+
+        console.log(gifs);
+    }
+
+
+    return (
+        <div>
+            <h3>{category}</h3>
+      
+        </div>
+    )
+}
+
+export default GifGrid
