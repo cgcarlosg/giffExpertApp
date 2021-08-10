18509aa73c3c0db4f59eee5afda27643190f64c6
cgcarlosg <cgcarlosg@hotmail.com>
2021-08-10 11:00:49 -0500

    fixing error

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,4 +1,3 @@
-
 export const getGifs = async (category) => {
 
     const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=zQrHcrBtgmuJwF6U00i5PbVddUQvyaRS`
@@ -13,7 +12,5 @@ export const getGifs = async (category) => {
             url: img.images?.downsized_medium.url
         }
     })
-
     return gifs;
-
 }
